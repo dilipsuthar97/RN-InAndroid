968b3d236ab655e1679434b4819d0aa9b30e6943
Dilip <35637341+dilipsuthar97@users.noreply.github.com>
2023-08-16 17:52:16 +0530

    RN setup in android

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 import React from 'react';
-import {AppRegistry, StyleSheet, Text, View} from 'react-native';
+import {AppRegistry, StatusBar, StyleSheet, Text, View} from 'react-native';
 
 const HelloWorld = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.hello}>Hello, World</Text>
+      <StatusBar backgroundColor={'blue'} />
+      <View
+        style={styles.header}
+      >
+        <Text style={styles.headerText}>Custom Header</Text>
+      </View>
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>Welcome to ReactNative</Text>
+      </View>
     </View>
   );
 };
 var styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  header: {
+    backgroundColor: 'blue',
+    height: 54,
     justifyContent: 'center',
+    paddingHorizontal: 16
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 18
+  },
+  wrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    color: 'black',
+    fontSize: 26,
+    textAlign: 'center',
   },
   hello: {
     fontSize: 20,
